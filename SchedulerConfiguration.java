1bf0cc3ddf03327b1813138634dcca461363de5b
sriikant <41874086+sriikant@users.noreply.github.com>
2020-03-20 10:12:29 +0530

    Create SchedulerConfiguration.java

---
+++ SchedulerConfiguration.java
@@ -0,0 +1,36 @@
+package idea.core.schedulers;
+
+import org.osgi.service.metatype.annotations.AttributeDefinition;
+import org.osgi.service.metatype.annotations.AttributeType;
+import org.osgi.service.metatype.annotations.ObjectClassDefinition;
+ 
+@ObjectClassDefinition(
+        name = "Experience AEM: SlingSchedulerConfiguration",
+        description = "Sling scheduler configuration"
+)
+public @interface SchedulerConfiguration {
+ 
+    @AttributeDefinition(
+            name = "Scheduler name",
+            description = "Name of the scheduler",
+            type = AttributeType.STRING)
+    public String schedulerName() default "Custom Sling Scheduler Configuration";
+ 
+    @AttributeDefinition(
+            name = "Enabled",
+            description = "True, if scheduler service is enabled",
+            type = AttributeType.BOOLEAN)
+    public boolean enabled() default false;
+ 
+    @AttributeDefinition(
+            name = "Cron Expression",
+            description = "Cron expression used by the scheduler",
+            type = AttributeType.STRING)
+    public String cronExpression() default "0/10 * * * * ?"; // runs every 10 seconds
+ 
+    @AttributeDefinition(
+            name = "Custom Path Parameter",
+            description = "Custom path parameter to be used by the scheduler",
+            type = AttributeType.STRING)
+    public String customPathParameter() default "/content/dam";
+}
